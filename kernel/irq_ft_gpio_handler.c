21b9c0036f9ac61c8e8271d21c934a49ea6122ac
fdeslandes <deslandesf@hotmail.com>
2013-11-28 15:32:52 +0100

    Adding parameters to set gpio in and out

--- kernel/irq_ft_gpio_handler.c
+++ kernel/irq_ft_gpio_handler.c
@@ -2,9 +2,15 @@
 #include <linux/module.h>
 #include <linux/gpio.h>
 
+// Module parameters
+static int ft_gpio_out = 22;
+static int ft_gpio_in = 23;
 
-#define FT_GPIO_OUT 22
-#define FT_GPIO_IN 23
+module_param(ft_gpio_out, int, 0);
+MODULE_PARM_DESC(ft_gpio_out, "Output GPIO number");
+module_param(ft_gpio_in, int, 0);
+MODULE_PARM_DESC(ft_gpio_in, "Input GPIO number");
+
 
 // Interruput handler 
 // Reverse gpio out when an interrupt occurs on gpio in
@@ -12,7 +18,7 @@ static irqreturn_t irq_ft_gpio_handler(int irq, void* ident)
 {
 	static int output_value;
 
-	gpio_set_value(FT_GPIO_OUT, output_value);
+	gpio_set_value(ft_gpio_out, output_value);
 
 	output_value = 1 - output_value;
 
@@ -27,38 +33,38 @@ static int __init module_loading(void)
 	printk(KERN_INFO "Loading ft_gpio_handler\n");
 
 	// Request GPIO in
-	if((err=gpio_request(FT_GPIO_IN, THIS_MODULE->name)) != 0){
+	if((err=gpio_request(ft_gpio_in, THIS_MODULE->name)) != 0){
 		return err;
 	}
 
 	// Request GPIO out
-	if((err=gpio_request(FT_GPIO_IN, THIS_MODULE->name)) != 0){
-		gpio_free(FT_GPIO_IN);
+	if((err=gpio_request(ft_gpio_in, THIS_MODULE->name)) != 0){
+		gpio_free(ft_gpio_in);
 		return err;
 	}
 
 	// Configure GPIO in
-	if((err=gpio_direction_input(FT_GPIO_OUT)) != 0){
-		gpio_free(FT_GPIO_IN);
-		gpio_free(FT_GPIO_OUT);
+	if((err=gpio_direction_input(ft_gpio_out)) != 0){
+		gpio_free(ft_gpio_in);
+		gpio_free(ft_gpio_out);
 		return err;
 	}
 
 	// Configure GPIO out
-	if((err=gpio_direction_output(FT_GPIO_IN, 1)) != 0){
-		gpio_free(FT_GPIO_IN);
-		gpio_free(FT_GPIO_OUT);
+	if((err=gpio_direction_output(ft_gpio_in, 1)) != 0){
+		gpio_free(ft_gpio_in);
+		gpio_free(ft_gpio_out);
 		return err;
 	}
 
 	// Install interrupt handler on GPIO in
-	if((err=request_irq(gpio_to_irq(FT_GPIO_IN),
+	if((err=request_irq(gpio_to_irq(ft_gpio_in),
 								irq_ft_gpio_handler,
 								IRQF_SHARED | IRQF_TRIGGER_RISING,
 								THIS_MODULE->name,
 								THIS_MODULE->name)) != 0){
-		gpio_free(FT_GPIO_IN);
-		gpio_free(FT_GPIO_OUT);
+		gpio_free(ft_gpio_in);
+		gpio_free(ft_gpio_out);
 		return err;
 	}
 
@@ -71,11 +77,11 @@ static void __exit module_unloading(void)
 	printk(KERN_INFO "Unloading gpio_ft_irq_handler\n");
 
 	// Free irq
-	free_irq(gpio_to_irq(FT_GPIO_IN), THIS_MODULE->name);
+	free_irq(gpio_to_irq(ft_gpio_in), THIS_MODULE->name);
 
 	// Free GPIO
-	gpio_free(FT_GPIO_IN);
-	gpio_free(FT_GPIO_OUT);
+	gpio_free(ft_gpio_in);
+	gpio_free(ft_gpio_out);
 }
 
 module_init(module_loading);
